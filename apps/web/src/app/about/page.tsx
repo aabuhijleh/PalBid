12ecac74328b5939ae733c19f81343953f6db1ff
aabuhijleh <aabuhijleh.dev@gmail.com>
2024-12-16 00:09:23 +0200

    feat: use server component in homepage

---
+++ apps/web/src/app/about/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { env } from "#/config/env";
+import { getMe } from "#/lib/client";
+
+export default function IndexPage() {
+  const [user, setUser] = useState<Awaited<ReturnType<typeof getMe>> | null>(
+    null,
+  );
+
+  useEffect(() => {
+    getMe()
+      .then(setUser)
+      .catch((err) => {
+        console.error("failed to fetch user", err);
+      });
+  }, []);
+
+  useEffect(() => {
+    void fetch(new URL("/users/test", env.NEXT_PUBLIC_API_URL).href).catch(
+      (err) => {
+        console.error("failed test", err);
+      },
+    );
+  }, []);
+
+  return (
+    <div>
+      <h1>Current user</h1>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+
+      <form
+        action={new URL("/users/sign-in", env.NEXT_PUBLIC_API_URL).href}
+        method="GET"
+      >
+        <input type="submit" value="Sign in" />
+      </form>
+
+      <form
+        action={new URL("/users/sign-out", env.NEXT_PUBLIC_API_URL).href}
+        method="POST"
+      >
+        <input type="submit" value="Sign out" />
+      </form>
+    </div>
+  );
+}
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,44 +1,12 @@
-"use client";
-
-import { useEffect } from "react";
 import { env } from "#/config/env";
-// import { getMe } from "#/lib/client";
+import { getMe } from "#/lib/client";
 
-export default function IndexPage() {
-  // const currentUser = await getMe();
-
-  useEffect(() => {
-    void fetch(new URL("/users/test", env.NEXT_PUBLIC_API_URL).href).catch(
-      (err) => {
-        console.error("failed", err);
-      },
-    );
-  }, []);
+export default async function IndexPage() {
+  const currentUser = await getMe();
 
   return (
     <div>
       <h1>Current user</h1>
-
-      <form
-        action={new URL("/users/sign-in", env.NEXT_PUBLIC_API_URL).href}
-        method="GET"
-      >
-        <input type="submit" value="Sign in" />
-      </form>
-
-      <form
-        action={new URL("/users/sign-out", env.NEXT_PUBLIC_API_URL).href}
-        method="GET"
-      >
-        <input type="submit" value="Sign out" />
-      </form>
-
-      {/* <form
-        action={new URL("/users/sign-out", env.NEXT_PUBLIC_API_URL).href}
-        method="POST"
-      >
-        <input type="submit" value="Sign out" />
-      </form>
       <pre>{JSON.stringify(currentUser, null, 2)}</pre>
 
       {currentUser ? (
@@ -55,7 +23,7 @@ export default function IndexPage() {
         >
           <input type="submit" value="Sign in" />
         </form>
-      )} */}
+      )}
     </div>
   );
 }
